186f2ecda3d61b73863f53cc30aec019d9d2ec8f
frazor97 <107914165+frazor97@users.noreply.github.com>
2022-06-25 10:00:39 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env_python
+
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+  def get(self):
+    self.response.write('Hello World')
+app = webapp2.WSGIApplication([
+      ('/', MainHandler)], debug=True)
